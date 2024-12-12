13f9267371b67e255633438173107f5d4e882fcd
Victor Alvarez <jokerprince726@gmail.com>
2024-12-12 00:24:37 -0600

    for render.com

--- medicamentos/apps.py
+++ medicamentos/apps.py
@@ -1,8 +1,4 @@
 from django.apps import AppConfig
-from .models import Medicamento, Venta  # Esto puede causar conflictos
 
 class MedicamentosConfig(AppConfig):
     name = 'medicamentos'
-
-    def ready(self):
-        from .models import Medicamento, Venta  # Duplicado
